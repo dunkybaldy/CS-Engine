43d1f52d6363ae1a3982a0bc23ae303d76771ee4
dunkybaldy <baldwin95@outlook.com>
2020-05-18 16:43:37 +0800

    Initial procedural generation

---
+++ source/Game.Logic/ProceduralGeneration/Initialiser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Logic.ProceduralGeneration
+{
+    public class Initialiser
+    {
+        public Initialiser()
+        {
+
+        }
+    }
+}
